6016719fc943ef13fe794023924e6642bab4527f
Martin Castillo <58961096+waskull@users.noreply.github.com>
2025-06-27 03:46:45 -0400

    added prod public api

--- app/apiURL.ts
+++ app/apiURL.ts
@@ -1,2 +1,2 @@
-const apiURL:string = process.env.NODE_ENV === "development" ? "http://localhost:8000/api/v1" : "https://mi-precio.onrender.com";
+const apiURL:string = process.env.NODE_ENV === "development" ? "http://localhost:8000/api/v1" : "https://mi-precio.onrender.com/api/v1";
 export default apiURL;
